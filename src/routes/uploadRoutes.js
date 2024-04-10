376f07061b63ef54c95e9419ccdee900453dae7b
José Augusto Freire <aquarela.design@gmail.com>
2024-04-10 02:16:21 -0300

    Add Upload Files

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -4,18 +4,35 @@ import multer from "multer";
 const router = express.Router();
 
 const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, "uploads/");
+  destination: function (req, file, cb) {
+    console.log("file", file);
+    return cb(null, `./public/files/${file.fieldname}/`);
   },
-  filename(req, file, cb) {
-    cb(
+  filename: function (req, file, cb) {
+    return cb(
       null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+      `${path.parse(file.originalname).name}-${Date.now()}${path.extname(
+        file.originalname
+      )}`
     );
   },
 });
 
 function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png|pdf|doc|docx|txt|xls|xlsx/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    return cb(
+      "only .jpg, .jpeg, .png, .pdf, .doc, .docx, .txt, .xls, .xlsx  extensions allowed"
+    );
+  }
+}
+
+function checkFileTypeImages(file, cb) {
   const filetypes = /jpg|jpeg|png/;
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   const mimetype = filetypes.test(file.mimetype);
@@ -23,19 +40,80 @@ function checkFileType(file, cb) {
   if (extname && mimetype) {
     return cb(null, true);
   } else {
-    cb("Images only!");
+    return cb("only .jpg, .jpeg, .png extensions allowed");
+  }
+}
+
+function checkFileTypeDocs(file, cb) {
+  const filetypes = /pdf|doc|docx|txt|xls|xlsx/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    return cb("only .pdf, .doc, .docx, .txt, .xls and xlsx extensions allowed");
   }
 }
 
 const upload = multer({
   storage,
   fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
+    return checkFileType(file, cb);
   },
 });
 
-router.post("/", upload.single("image"), (req, res) => {
-  res.send(`/${req.file.path}`);
+const uploadImg = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    return checkFileTypeImages(file, cb);
+  },
+});
+
+const uploadDoc = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    return checkFileTypeDocs(file, cb);
+  },
+});
+
+const uploadImgs = uploadImg.single("users");
+router.post("/users", function (req, res) {
+  uploadImgs(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      res.status(400).json({ message: "", error: err });
+    } else if (err) {
+      res.status(400).json({ message: "", error: err });
+    } else {
+      res.status(200).json({ message: `/${req?.file?.path}`, error: "" });
+    }
+  });
+});
+
+const uploadDocs = uploadDoc.single("docs");
+router.post("/docs", function (req, res) {
+  uploadDocs(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      res.status(400).json({ message: "", error: err });
+    } else if (err) {
+      res.status(400).json({ message: "", error: err });
+    } else {
+      res.status(200).json({ message: `/${req?.file?.path}`, error: "" });
+    }
+  });
+});
+
+const uploads = upload.single("library");
+router.post("/library", function (req, res) {
+  uploads(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      res.status(400).json({ message: "", error: err });
+    } else if (err) {
+      res.status(400).json({ message: "", error: err });
+    } else {
+      res.status(200).json({ message: `/${req?.file?.path}`, error: "" });
+    }
+  });
 });
 
 export default router;
